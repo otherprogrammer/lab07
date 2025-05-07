5e6851143eaf35a99caf7aac039b92b7e8c9d6b6
Mage0w0 <mattiasmarquez21@gmail.com>
2025-05-06 23:03:47 -0500

    Primer commit

---
+++ app/src/main/java/com/example/datossinmvvm/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.datossinmvvm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.datossinmvvm.ui.theme.DatosSinMVVMTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DatosSinMVVMTheme {
+                ScreenUser()
+            }
+        }
+    }
+}
